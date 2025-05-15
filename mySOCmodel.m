714089fd5f3fa09e1298f66fb73c6e111a6561ce
red-obsidian12 <davide.rizzi01@universitadipavia.it>
2025-05-15 12:56:39 +0200

    funzione finale

---
+++ mySOCmodel.m
@@ -0,0 +1,20 @@
+function soc_pred = mySOCmodel(voltage_vec, temp_vec)
+    load model.mat
+    
+    gradoPolinomio=length(thetaModel)-1;
+    n = length(voltage_vec);
+    phi = ones(n, 1); % Partiamo con il termine di grado 0
+    
+    for grado = 1:gradoPolinomio
+        for j = 0:grado
+            k = grado - j;
+            % aggiungiamo X1^j * X2^k come nuova colonna
+            nuovaColonna = (voltage_vec.^j) .* (temp_vec.^k);
+            phi = [phi, nuovaColonna];
+            
+        end
+    end
+
+    soc_pred = phi * thetaModel;
+
+end
